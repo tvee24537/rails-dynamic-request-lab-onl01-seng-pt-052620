ac6e37796e313b6e66651fa70800e7d2af5e5603
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-09-28 21:44:19 +0000

    Automatically backed up by Learn

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -2,4 +2,12 @@ class StudentsController < ApplicationController
   def index
     @students = Student.all
   end
+  def show
+  end
+
+  private
+
+    def set_student
+      @student = Student.find(params[:id])
+    end
 end
